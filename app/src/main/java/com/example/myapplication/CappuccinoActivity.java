a284f9a23e032343883da61fa070b49bcfe63e2b
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-04-11 09:10:56 +0200

    Made a change in UI

--- app/src/main/java/com/example/myapplication/CappuccinoActivity.java
+++ app/src/main/java/com/example/myapplication/CappuccinoActivity.java
@@ -5,6 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +17,7 @@ import butterknife.ButterKnife;
 public class CappuccinoActivity extends Activity {
     //@BindView(R.id.button) Button order;
     Button Register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/myapplication/DrinksActivity.java
+++ app/src/main/java/com/example/myapplication/DrinksActivity.java
@@ -25,5 +25,6 @@ public class DrinksActivity extends Activity {
 
             }
         });
+
     }
 }
