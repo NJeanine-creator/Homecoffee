895b32a135a934ec95b7ecf30742991ad0cfe83e
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-04-11 12:17:21 +0200

    Add a toast to the app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -40,30 +42,40 @@ public class MainActivity extends AppCompatActivity {
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    Intent intent = new Intent(MainActivity.this, DrinksActivity.class);
-                    startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Handler objHandler = new Handler();
+                objHandler.post(new Runnable() {
 
-                }
-                if (position == 1) {
-                    Intent intent = new Intent(MainActivity.this, CappuccinoActivity.class);
-                    startActivity(intent);
-                }
-                if (position == 2) {
-                    Intent intent = new Intent(MainActivity.this, FilterActivity.class);
-                    startActivity(intent);
-                }
-            }
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        Toast.makeText(getBaseContext(), "You can make order now", Toast.LENGTH_LONG).show();
+                    }
+                    {
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this, DrinksActivity.class);
+                            startActivity(intent);
+
+                        }
+                        if (position == 1) {
+                            Intent intent = new Intent(MainActivity.this, CappuccinoActivity.class);
+                            startActivity(intent);
+                        }
+                        if (position == 2) {
+                            Intent intent = new Intent(MainActivity.this, FilterActivity.class);
+                            startActivity(intent);
+                        }
+                    }
  /* public void OnItemClick(AdapterView<?> listView, View v, int position,long id ){
 
  } */
+                });
+            }
         };
+
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
-
-    }
-
+                }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
