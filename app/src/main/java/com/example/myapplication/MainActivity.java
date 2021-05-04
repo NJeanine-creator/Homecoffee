393507c45ec52c00990c93de37d79ca4df424808
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-05-04 22:34:14 +0200

    Add animation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.myapplication.R;
 
 import butterknife.BindView;
@@ -14,24 +19,37 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity  {
 
+    private TextView mTextView;
+
     @BindView(R.id.btnGo) Button mGo;
     @BindView(R.id.btnsearch) Button msearch;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initializing the ViewPager Object
+        mViewPager = (ViewPager)findViewById(R.id.viewPagerMain);
+
+        //Initializing the ViewPagerAdapter
+        mViewPagerAdapter = new ViewPagerAdapter(MainActivity.this, images);
+
+        //Adding the Adapter to the ViewPager
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+        mTextView = findViewById(R.id.hometitle);
 
         ButterKnife.bind(this);
         FragmentManager fm = getSupportFragmentManager();
-        //DailyChoose dailyChoose = new DailyChoose ();
-        // PromptActivity promptActivity=new PromptActivity();
-        // promptActivity.show(fm, "Sample Fragment");
 
         mGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,HomeActivity.class);
                 startActivity(intent);
+                YoYo.with(Techniques.Tada)
+                        .duration(1000)
+                        .repeat(2)
+                        .playOn(mTextView);
             }
         });
         msearch.setOnClickListener(new View.OnClickListener() {
@@ -42,4 +60,5 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
     }
+
 }
